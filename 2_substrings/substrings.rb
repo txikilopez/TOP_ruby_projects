7c3f519b482ec8937c55e8895725962eea727b79
Antonio <atroncoso10@gmail.com>
2024-08-01 14:32:07 -0400

    allow interaction to input words and list of words

---
+++ 2_substrings/substrings.rb
@@ -0,0 +1,22 @@
+
+def substrings(phrase, word_collection)
+  word_collection.reduce(Hash.new(0)) do |accum, word|
+    if phrase.include?(word)
+      accum[word] = accum[word] + 1
+    end
+    accum
+  end
+end
+
+# testblock
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# string_test = "below"
+
+puts "Enter a phrase you want to check:"
+string_test = gets.chomp
+
+puts "Enter a list of words separated by a comma"
+dictionary = gets.chomp
+dictionary = dictionary.split(", ")
+
+p substrings(string_test, dictionary)
