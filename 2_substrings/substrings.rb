2ceb554f99ec5b98d95d6593a271ee8ebbb12076
Antonio <atroncoso10@gmail.com>
2024-08-01 16:50:38 -0400

    fixes the word count

--- 2_substrings/substrings.rb
+++ 2_substrings/substrings.rb
@@ -1,25 +1,27 @@
-
+require "pry-byebug"
 def substrings(phrase, word_collection)
   phrase = phrase.downcase
   word_collection = word_collection.map {|word| word.downcase}
+  # binding.pry
   word_collection.reduce(Hash.new(0)) do |accum, word|
     if phrase.include?(word)
-      accum[word] = accum[word] + 1
+      accum[word] = phrase.scan(word).length
     end
     accum
   end
+  
 end
 
 # testblock
-# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-# string_test = "below"
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+string_test = "Howdy partner, sit down! How's it going?"
 
-puts "Enter a phrase you want to check:"
-string_test = gets.chomp
+# puts "Enter a phrase you want to check:"
+# string_test = gets.chomp
 
-puts "Enter a list of words separated by a comma"
-dictionary = gets.chomp
-dictionary = dictionary.split(", ")
+# puts "Enter a list of words separated by a comma"
+# dictionary = gets.chomp
+# dictionary = dictionary.split(", ")
 
-puts "The phrase includes the following subset of words in the word collection" 
+# puts "The phrase includes the following subset of words in the word collection" 
 puts substrings(string_test, dictionary)
