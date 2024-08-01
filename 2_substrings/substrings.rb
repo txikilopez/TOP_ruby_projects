626ecd52e792ca5356cb0cd4079ee4d7a275f5ce
Antonio <atroncoso10@gmail.com>
2024-08-01 16:51:07 -0400

    right version uploaded

--- 2_substrings/substrings.rb
+++ 2_substrings/substrings.rb
@@ -13,15 +13,15 @@ def substrings(phrase, word_collection)
 end
 
 # testblock
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-string_test = "Howdy partner, sit down! How's it going?"
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# string_test = "Howdy partner, sit down! How's it going?"
 
-# puts "Enter a phrase you want to check:"
-# string_test = gets.chomp
+puts "Enter a phrase you want to check:"
+string_test = gets.chomp
 
-# puts "Enter a list of words separated by a comma"
-# dictionary = gets.chomp
-# dictionary = dictionary.split(", ")
+puts "Enter a list of words separated by a comma"
+dictionary = gets.chomp
+dictionary = dictionary.split(", ")
 
 # puts "The phrase includes the following subset of words in the word collection" 
 puts substrings(string_test, dictionary)
