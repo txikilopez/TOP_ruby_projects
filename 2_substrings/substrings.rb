0965bf51d1be2b88124dbfc0c28f2c6a21dd8514
Antonio <atroncoso10@gmail.com>
2024-08-01 16:55:30 -0400

    version that works, final upload.

--- 2_substrings/substrings.rb
+++ 2_substrings/substrings.rb
@@ -1,8 +1,6 @@
-require "pry-byebug"
 def substrings(phrase, word_collection)
   phrase = phrase.downcase
   word_collection = word_collection.map {|word| word.downcase}
-  # binding.pry
   word_collection.reduce(Hash.new(0)) do |accum, word|
     if phrase.include?(word)
       accum[word] = phrase.scan(word).length
@@ -12,10 +10,6 @@ def substrings(phrase, word_collection)
   
 end
 
-# testblock
-# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-# string_test = "Howdy partner, sit down! How's it going?"
-
 puts "Enter a phrase you want to check:"
 string_test = gets.chomp
 
@@ -23,5 +17,13 @@ puts "Enter a list of words separated by a comma"
 dictionary = gets.chomp
 dictionary = dictionary.split(", ")
 
-# puts "The phrase includes the following subset of words in the word collection" 
+puts "The phrase includes the following subset of words in the word collection" 
 puts substrings(string_test, dictionary)
+
+
+# testblock
+# dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# string_test = "Howdy partner, sit down! How's it going?"
+# puts "The phrase includes the following subset of words in the word collection" 
+# puts substrings(string_test, dictionary)
+#
